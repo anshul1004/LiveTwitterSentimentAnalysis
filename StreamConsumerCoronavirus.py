16400981895f40eab31ca7775322923e8a94e88b
anshul1004 <anshul.pardhi@gmail.com>
2020-05-10 19:25:22 -0500

    Pushed whole project

---
+++ StreamConsumerCoronavirus.py
@@ -0,0 +1,73 @@
+"""
+Author: Anshul Pardhi
+Spark streaming using Kafka consumer which continuously collects filtered tweets,
+performs sentiment analysis and sends the results to elastic search
+"""
+from kafka import KafkaConsumer
+import json
+from elasticsearch import Elasticsearch
+from textblob import TextBlob
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+es = Elasticsearch()
+
+
+def main():
+    """
+    main function initiates a kafka consumer, initialize the tweetdata database.
+    Consumer consumes tweets from producer extracts features, cleanses the tweet text,
+    calculates sentiments and loads the data into postgres database
+    """
+    # set-up a Kafka consumer
+    consumer = KafkaConsumer("twitter_coronavirus")
+    for msg in consumer:
+
+        dict_data = json.loads(msg.value)
+
+        # Create a blob
+        tweet_blob = TextBlob(dict_data["text"])
+        tweet = dict_data["text"]
+
+        print(tweet_blob)  # Print the tweet text
+
+        # Calculate blob sentiment
+        print(format(tweet_blob.sentiment))
+        polarity = tweet_blob.sentiment.polarity
+        if polarity > 0:
+            blob_sentiment = "Positive"
+        elif polarity < 0:
+            blob_sentiment = "Negative"
+        else:
+            blob_sentiment = "Neutral"
+
+        # Calculate vader sentiment
+        sid = SentimentIntensityAnalyzer()
+        ss = sid.polarity_scores(tweet)
+        for k in sorted(ss):
+            print('{0}: {1}, '.format(k, ss[k]), end='')
+            if k == 'compound':
+                if ss[k] >= 0.05:
+                    vader_sentiment = "Positive"
+                elif ss[k] <= -0.05:
+                    vader_sentiment = "Negative"
+                else:
+                    vader_sentiment = "Neutral"
+
+        print()
+        print("Blob Sentiment: ", blob_sentiment)
+        print("Vader Sentiment: ", vader_sentiment)
+        print()
+
+        # add text and sentiment info to elasticsearch
+        es.index(index="tweet_coronavirus",
+                 doc_type="test-type",
+                 body={"author": dict_data["user"]["screen_name"],
+                       "date": dict_data["created_at"],
+                       "message": dict_data["text"],
+                       "blob_sentiment": blob_sentiment,
+                       "vader_sentiment": vader_sentiment})
+        print('\n')
+
+
+if __name__ == "__main__":
+    main()
---
+++ StreamProducerTrump.py
@@ -0,0 +1,49 @@
+"""
+Author: Anshul Pardhi
+Scrapper (Kafka Producer) which continuously extracts tweets with a given hashtag
+"""
+from kafka import KafkaProducer
+import tweepy
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
+# TWITTER API CONFIGURATIONS
+consumer_key = "n6d7hyc6mNR6vcPsHqsLC6gCV"
+consumer_secret = "mVve8URjCgeqD29tUnfGHfuovWyZIH26JKF4eIKIb9NkeuzEof"
+access_token = "2800332947-jt17YAXNXTmiZjcRMCdtaWbZ5WkHVB4wvZitkkQ"
+access_secret = "dr0JnsEcxISr8doTxpUSHd2qv1gVasce4uPp9OpXASCSl"
+
+# TWITTER API AUTH
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
+
+
+# Twitter Stream Listener
+class KafkaPushListener(StreamListener):
+    def __init__(self):
+        # localhost:9092 = Default Zookeeper Producer Host and Port Adresses
+        self.producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+
+    # Get Producer that has topic name is Twitter
+    # self.producer = self.client.topics[bytes("twitter")].get_producer()
+
+    def on_data(self, data):
+        # Producer produces data for consumer
+        # Data comes from Twitter
+        self.producer.send("twitter_trump", data.encode('utf-8'))
+        print(data)
+        return True
+
+    def on_error(self, status):
+        print(status)
+        return True
+
+
+# Twitter Stream Config
+twitter_stream = Stream(auth, KafkaPushListener())
+
+# Produce Data that has trump and coronavirus hashtag (Tweets)
+twitter_stream.filter(track=['#trump'])
